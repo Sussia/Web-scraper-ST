e6077eddf6dd07242a4ed0898ed855c7b7131013
Artamonov Aleksandr <artamonovalex18@gmail.com>
2020-04-10 23:21:14 +0300

    Add test on parsing function

---
+++ scraper-cliTests/ParsingTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using scraper_cli;
+
+namespace scraper_cliTests
+{
+    [TestClass]
+    public class ParsingTests
+    {
+        [TestMethod]
+        public void ParsePage()
+        {
+            WebScraper scraper = new WebScraper();
+            string title = "My test title";
+            string testPageContent = $"<html><title>{title}</title></html>";
+            ParsingRule rule = new ParsingRule("<title>", "</title>", title);
+            Dictionary<string, string> scrapedValues =
+                scraper.ParsePage(testPageContent, new List<ParsingRule>() { rule });
+            string scrapedTitle = scrapedValues[rule.title];
+
+            Assert.AreEqual(title, scrapedTitle);
+        }
+    }
+}
