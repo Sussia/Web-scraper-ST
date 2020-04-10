a655cb26cc20559c66da494b252cb0e73a474c3c
Artamonov Aleksandr <artamonovalex18@gmail.com>
2020-04-10 23:16:49 +0300

    Add MyConsole class

---
+++ scraper-cli/IConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace scraper_cli
+{
+    public interface IConsole
+    {
+        public void WriteLine(string message);
+        public void WriteLine();
+        public void Write(string message);
+        public string ReadLine();
+        public void Clear();
+    }
+}
---
+++ scraper-cli/MyConsole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace scraper_cli
+{
+    public class MyConsole : IConsole
+    {
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public virtual string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WriteLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
