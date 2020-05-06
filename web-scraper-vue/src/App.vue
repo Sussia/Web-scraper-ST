c7391d59a97a5d47eb48d60c26c557b9251d3795
Aleksandr Artamonov <artamonovalex18@gmail.com>
2020-05-06 13:23:56 +0300

    Add extractvalue function

--- web-scraper-vue/src/App.vue
+++ web-scraper-vue/src/App.vue
@@ -1,19 +1,66 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <ol>
+        <li v-for="rule in rules" :key="rule.id">
+          <p>{{ rule.title }}: {{ rule.prefix }} - {{ rule.suffix }}</p>
+        </li>
+      </ol>
+    </div>
+    <input type="text" placeholder="Введите текст" v-model="textToParse">
+    <button @click="parseText">Извлечь значения</button>
+    <p v-if="showExtractedValues">{{extractedValue.title}}: {{extractedValue.value}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      showExtractedValues: false,
+      textToParse: '',
+      rules: [
+        {
+          title: 'MyTestTitle', suffix: '</title>', prefix: '<title>', id: 1
+        },
+        {
+          title: 'MyTestTitle2', suffix: '</title>', prefix: '<title>', id: 2
+        }
+      ],
+      extractedValue: {
+        title: '',
+        value: ''
+      }
+    }
+  },
+  methods: {
+    parseText: function() {
+      function extractValue(text, prefix, suffix) {
+        let matchResult = text.match(new RegExp(prefix + '(.*)' + suffix))
+        if (matchResult != null) {
+          return matchResult[1]
+        } else {
+          return null
+        }
+      }
+      let rule = this.rules[0]
+      let value = extractValue(this.textToParse, rule.prefix, rule.suffix)
+      if (value != null) {
+        this.extractedValue =  {
+          title: rule.title,
+          value: extractValue(this.textToParse, rule.prefix, rule.suffix)
+        }
+        this.showExtractedValues = true
+      } else {
+        this.showExtractedValues = false
+      }
+    }
   }
 }
+
 </script>
 
 <style>
