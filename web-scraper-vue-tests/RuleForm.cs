01cfe049126c8ae2f422575c5baf18eaed5105be
Aleksandr Artamonov <artamonovalex18@gmail.com>
2020-05-26 15:54:04 +0300

    Create RuleForm page object

---
+++ web-scraper-vue-tests/RuleForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace web_scraper_vue_tests
+{
+    public class RuleForm
+    {
+        public IWebElement form;
+
+        public RuleForm(IWebElement form)
+        {
+            this.form = form;
+        }
+
+        public IWebElement title {
+            get {
+                return form.FindElement(By.CssSelector("div.title-field")).FindElement(By.CssSelector("input"));
+            }
+        }
+
+        public IWebElement description
+        {
+            get
+            {
+                return form.FindElement(By.CssSelector("div.description-field")).FindElement(By.CssSelector("input"));
+            }
+        }
+
+        public IWebElement perfix
+        {
+            get
+            {
+                return form.FindElement(By.CssSelector("div.prefix-field")).FindElement(By.CssSelector("input"));
+            }
+        }
+
+        public IWebElement suffix
+        {
+            get
+            {
+                return form.FindElement(By.CssSelector("div.suffix-field")).FindElement(By.CssSelector("input"));
+            }
+        }
+
+        public IWebElement saveButton
+        {
+            get
+            {
+                return form.FindElement(By.CssSelector("button.save-button"));
+            }
+        }
+
+        public IWebElement cancelButton
+        {
+            get
+            {
+                return form.FindElement(By.CssSelector("div.title-field")).FindElement(By.CssSelector("input"));
+            }
+        }
+    }
+}
--- web-scraper-vue-tests/RuleManagementTests.cs
+++ web-scraper-vue-tests/RuleManagementTests.cs
@@ -12,46 +12,39 @@ namespace web_scraper_vue_tests
         public void Setup()
         {
             driver = new SafariDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://localhost:8080";
         }
         
         [Test]
         public void RuleCreationTest()
         {
-            driver.Manage().Window.Maximize();
-            driver.Url = "http://localhost:8080";
-
             var cards = driver.FindElements(By.CssSelector("div.rule-card"));
-
             Assert.AreEqual(1, cards.Count);
                 
-            var searchInput = driver.FindElement(By.CssSelector("#plus-card > button"));
-            searchInput.SendKeys(Keys.Enter);
+            var plusButton = driver.FindElement(By.CssSelector("#plus-card > button"));
+            plusButton.SendKeys(Keys.Enter);
 
-            var ruleCreationForm = driver.FindElement(By.CssSelector("#plus-card > form"));
+            RuleForm creationForm = new RuleForm(driver.FindElement(By.CssSelector("#plus-card > form")));
 
-            var title = ruleCreationForm.FindElement(By.CssSelector("div.title-field")).FindElement(By.CssSelector("input"));
-            title.SendKeys("Test title");
+            string testTitle = "Test title";
 
-            var description = ruleCreationForm.FindElement(By.CssSelector("div.description-field")).FindElement(By.CssSelector("input"));
-            description.SendKeys("Some description");
+            creationForm.title.SendKeys(testTitle);
+            creationForm.description.SendKeys("Some description");
+            creationForm.perfix.SendKeys("prefix");
+            creationForm.suffix.SendKeys("suffix");
 
-            var prefix = ruleCreationForm.FindElement(By.CssSelector("div.prefix-field")).FindElement(By.CssSelector("input"));
-            prefix.SendKeys("prefix");
+            var saveButton = creationForm.saveButton;
 
-            var suffix = ruleCreationForm.FindElement(By.CssSelector("div.suffix-field")).FindElement(By.CssSelector("input"));
-            suffix.SendKeys("suffix");
-
-            var saveButton = ruleCreationForm.FindElement(By.CssSelector("button.save-button"));
             Assert.AreEqual(true, saveButton.Enabled);
 
             saveButton.SendKeys(Keys.Enter);
-
 
             cards = driver.FindElements(By.CssSelector("div.rule-card"));
             Assert.AreEqual(2, cards.Count);
 
             var secondCardTitle = cards[1].FindElement(By.CssSelector("div.v-card__title > span"));
-            Assert.AreEqual("Test title", secondCardTitle.Text);
+            Assert.AreEqual(testTitle, secondCardTitle.Text);
 
         }
 
