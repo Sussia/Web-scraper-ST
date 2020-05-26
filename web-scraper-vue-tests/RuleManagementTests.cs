6aafa67fc7b9fc0ec10f58cba038837836f4f69c
Aleksandr Artamonov <artamonovalex18@gmail.com>
2020-05-27 01:19:52 +0300

    Add comments

--- web-scraper-vue-tests/RuleManagementTests.cs
+++ web-scraper-vue-tests/RuleManagementTests.cs
@@ -24,51 +24,60 @@ namespace web_scraper_vue_tests
         [Test]
         public void RuleControlsTest()
         {
+            // 1.	Проверка количества правил при первоначальной загрузке страницы
             var cards = driver.FindElements(By.CssSelector("div.rule-card"));
             Assert.AreEqual(1, cards.Count);
-                
+            // 2.	Нажатие кнопки добавления правила
             var plusButton = driver.FindElement(By.CssSelector("#plus-card > button"));
             plusButton.SendKeys(Keys.Enter);
-
+            // 3.	Проверка открытия формы для создания правила
             RuleForm creationForm = new RuleForm(driver.FindElement(By.CssSelector("#plus-card > form")));
 
             string testTitle = "Test title";
             string prefix = "<prefix>";
             string suffix = "<suffix>";
-
+            // 4.	Заполнение необходимых полей
             creationForm.title.SendKeys(testTitle);
             creationForm.description.SendKeys("Some description");
             creationForm.perfix.SendKeys(prefix);
             creationForm.suffix.SendKeys(suffix);
 
             var saveButton = creationForm.saveButton;
-
+            // 5.	Проверка активности кнопки сохранения
             Assert.AreEqual(true, saveButton.Enabled);
-
+            // 6.	Нажатие на кнопку «Сохранить»
             saveButton.SendKeys(Keys.Enter);
 
+            // 7.	Проверка присутствия нового правила на странице и соответствия его введенным данным
             cards = driver.FindElements(By.CssSelector("div.rule-card"));
             Assert.AreEqual(2, cards.Count);
-
             var newCard = new RuleCard(cards[1]);
-
             Assert.AreEqual(testTitle, newCard.title);
 
+            // 8.	Нажатие на кнопку отображения подробностей
             newCard.expandButton.SendKeys(Keys.Enter);
+            // 9.	Проверка отображения подробностей
             Assert.AreEqual($"Prefix: {prefix}Suffix: {suffix}", newCard.text);
+            // 10.	Нажатие на кнопку скрытия подробностей
             newCard.expandButton.SendKeys(Keys.Enter);
 
+            // 11.	Нажатие на кнопку редактирование правила
             newCard.editButton.SendKeys(Keys.Enter);
             var editForm = newCard.editForm;
+            // 12.	Заполнение формы редактирования правила
             string newTitle = "New test title";
             editForm.title.SendKeys(Keys.Command + "a");
             editForm.title.SendKeys(Keys.Backspace);
             editForm.title.SendKeys(newTitle);
+            // 13.	Нажатие на кнопку «Сохранить»
             editForm.saveButton.SendKeys(Keys.Enter);
+            // 14.	Проверка соответствия контента страницы внесенным изменениям
             newCard = new RuleCard(driver.FindElements(By.CssSelector("div.rule-card"))[1]);
             Assert.AreEqual(newTitle, newCard.title);
 
+            // 15.	Нажатие на кнопку удаления правила
             newCard.deleteButton.SendKeys(Keys.Enter);
+            // 16.	Проверка отсутствия правила на странице
             cards = driver.FindElements(By.CssSelector("div.rule-card"));
             Assert.AreEqual(1, cards.Count);
 
@@ -86,31 +95,41 @@ namespace web_scraper_vue_tests
                     suffix = "</test>"
                 }
             };
+
+            // 1.	Создание файла с правилом в формате json
             string path = Path.Combine(homeDirectory, "Desktop/test.json");
             File.WriteAllText(path, JsonSerializer.Serialize(rules));
+
+            // 2.	Нажатие на кнопку «Загрузить правила»
+            // 3.	Указание пути к файлу
             var fileInput = driver.FindElement(By.CssSelector("input[type=\"file\"]"));
             fileInput.SendKeys(path);
 
+            // 4.	Проверка соответствия контента страницы загружаемому контенту
             var cards = driver.FindElements(By.CssSelector("div.rule-card"));
             Assert.AreEqual(2, cards.Count);
-
             var newCard = new RuleCard(cards[1]);
             Assert.AreEqual(testTitle, newCard.title);
 
+            // 5.	Удаление файла
             File.Delete(path);
         }
 
         [Test]
         public void DownloadRuleTest()
         {
+            // 1.	Нажатие на кнопку «Скачать правила»
+            // 2.	Сохранение файла
             var downloadButton = driver.FindElement(By.CssSelector("a.download-button"));
             downloadButton.SendKeys(Keys.Enter);
             Thread.Sleep(2000);
+
+            // 3.	Проверка соответствия скачанного контента предполагаемому контенту
             string path = Path.Combine(homeDirectory, "Downloads/parsing_rules.json");
             var rules = JsonSerializer.Deserialize<ParsingRule[]>(File.ReadAllText(path));
-
             Assert.AreEqual("Title", rules[0].title);
 
+            // 4.	Удаление файла
             File.Delete(path);
         }
 
--- web-scraper-vue-tests/ScrapingTests.cs
+++ web-scraper-vue-tests/ScrapingTests.cs
@@ -19,12 +19,14 @@ namespace web_scraper_vue_tests
             driver = new SafariDriver();
             driver.Manage().Window.Maximize();
             driver.Url = "http://localhost:8080";
+            // 1.	Нажатие на вкладку «Скрейпинг»
             driver.FindElement(By.CssSelector("#scraping-tab")).SendKeys(Keys.Enter);
         }
 
         [Test]
         public void LinksControlTest()
         {
+            // 2.	Проверка отображения вкладки скрейпинга
             var scrapingComponent = driver.FindElement(By.CssSelector("#scraping-component"));
             Assert.AreEqual(true, scrapingComponent.Displayed);
 
@@ -33,20 +35,25 @@ namespace web_scraper_vue_tests
 
             var urlContainers = driver.FindElements(By.CssSelector("div.url-field"));
 
+            // 3.	Заполнение первого адреса
             urlContainers[0].FindElement(By.CssSelector("input")).SendKeys(firstUrl);
 
+            // 4.	Проверка его отображения на странице
             urlContainers = driver.FindElements(By.CssSelector("div.url-field"));
             Assert.AreEqual(2, urlContainers.Count);
 
+            // 5.	Заполнение второго адреса
             urlContainers[1].FindElement(By.CssSelector("input")).SendKeys(secondUrl);
 
+            // 6.	Проверка его отображения на странице
             urlContainers = driver.FindElements(By.CssSelector("div.url-field"));
             Assert.AreEqual(3, urlContainers.Count);
 
+            // 7.	Нажатие на кнопку удаления адреса из списка напротив первого адреса
             var removeButtons = driver.FindElements(By.CssSelector("button.remove-url-button"));
-
             removeButtons[0].SendKeys(Keys.Enter);
 
+            // 8.	Проверка оставшегося адреса на равенство второму введенному
             urlContainers = driver.FindElements(By.CssSelector("div.url-field"));
             Assert.AreEqual(2, urlContainers.Count);
             Assert.AreEqual(secondUrl, urlContainers[0].FindElement(By.CssSelector("input")).GetAttribute("value"));
@@ -55,31 +62,43 @@ namespace web_scraper_vue_tests
         [Test]
         public void ScrapingTest()
         {
+            // 2.	Создание файла со списком адресов для скрейпинга
             var links = new string[] { "https://example.com" };
             string path = Path.Combine(homeDirectory, "Desktop/test.json");
             File.WriteAllText(path, JsonSerializer.Serialize(links));
+
+            // 3.	Нажатие на кнопку «Загрузить адреса»
+            // 4.	Указание пути к файлу с адресами
             var fileInput = driver.FindElement(By.CssSelector("input[type=\"file\"]"));
             fileInput.SendKeys(path);
 
+            // 5.	Нажатие на кнопку «Получить контент»
             driver.FindElement(By.CssSelector("#scrape-button")).SendKeys(Keys.Enter);
             Thread.Sleep(5000);
 
+            // 6.	Проверка соответствия отображенного контента в таблице предполагаемому
             var dataTable = driver.FindElement(By.CssSelector("#data-table"));
-
             var rows = dataTable.FindElements(By.CssSelector("td"));
             Assert.AreEqual(1, rows.Count);
             Assert.AreEqual("Example Domain", rows[0].Text);
 
+            // 7.	Нажатие на кнопку «Скачать таблицу»
             var downloadButton = driver.FindElement(By.CssSelector("a.download-button"));
             downloadButton.SendKeys(Keys.Enter);
             Thread.Sleep(2000);
+
+            // 8.	Проверка присутствия файла с таблицей на компьютере
             string tablePath = Path.Combine(homeDirectory, "Downloads/scraped_values.csv");
             Assert.AreEqual(true, File.Exists(tablePath));
 
+            // 9.	Нажатие на кнопку «Очистить таблицу»
             driver.FindElement(By.CssSelector("#clear-table-button")).SendKeys(Keys.Enter);
+
+            // 10.	Проверка отсутствия таблицы на странице
             var dataTables = driver.FindElements(By.CssSelector("#data-table"));
             Assert.AreEqual(0, dataTables.Count);
 
+            // 11.	Удаления файла с адресами и файла с таблицей
             File.Delete(path);
             File.Delete(tablePath);
 
